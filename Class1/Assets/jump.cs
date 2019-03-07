b556d8323f80b95d99ee3b833cb799c1cfd306c6
Ruen_note <ruen346@gmail.com>
2019-03-07 16:38:31 +0900

    겜인페1번째 수업

---
+++ Class1/Assets/jump.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class jump : MonoBehaviour {
+    float jumpspeed = 5.0f;
+    GameObject cube;
+
+	// Use this for initialization
+	void Start () {
+        cube = GameObject.Find("Cube");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+   public void cubejump()
+   {
+        cube.GetComponent<Rigidbody>().velocity = Vector3.up * jumpspeed;
+   }
+}
