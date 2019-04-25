fd5bb1eee099a3c7d163b0a8d1a937347b07200e
Ruen_note <ruen346@gmail.com>
2019-04-25 16:51:19 +0900

    4/25

---
+++ DandD/Assets/Draggable.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    private GameObject draggingobject; // 드래그 중인 아이콘의 게임 오브젝트
+    private RectTransform canvasRectTransform; // 캔버스의 렉트 트렌스폼
+
+    private void UpdateDraggingobjectPos(PointerEventData pointerEventData) // 드래그 조작중인 아이콘의 위치를 갱신 및 설정하는 메소드
+    {
+        if(draggingobject != null) // 드래그 중일때
+        {
+            Vector3 ScreenPos = pointerEventData.position; // 드래그 중인 아이콘의 스크린좌표를 가져온다.
+
+            Camera camera = pointerEventData.pressEventCamera;// 스크린 좌표를 월드 좌표로 변환
+
+            Vector3 newPos;
+            if(RectTransformUtility.ScreenPointToWorldPointInRectangle(canvasRectTransform, ScreenPos, camera, out newPos)) // 드래그 중인 아이콘의 위치를 설정
+            {
+                draggingobject.transform.position = newPos;
+                draggingobject.transform.rotation = canvasRectTransform.rotation;
+            }
+        }
+    }
+
+    public void OnBeginDrag(PointerEventData pointerEventData)// 드래그 조작이 시작될때 발생하는 이벤트 메소드
+    {
+        if(draggingobject != null) // 만약 이전에 드래그하던 객체가 남아있을 경우 지운다
+        {
+            Destroy(draggingobject);
+        }
+
+        Image sourceImage = GetComponent<Image>(); // 드래그하는 본래 아이콘의 이미지 컴포넌트를 가져온다
+
+        draggingobject = new GameObject("Dragging object"); // 드래그 중인 아이콘의 게임 오브젝트를 생성합니다
+
+        draggingobject.transform.SetParent(sourceImage.canvas.transform); // 본래 아이콘의 켄버스의 자식요소로 종속시키고 화면 맨 앞쪽에 표시한다
+        draggingobject.transform.SetAsLastSibling();
+        draggingobject.transform.localScale = Vector3.one;
+
+        // 캔버스 그룹 컴포넌트의 블록 레이캐스트 속성을 이용해서
+        // 레이캐스트가 이동중에 막히지 않도록 설정
+        CanvasGroup canvasGroup = draggingobject.AddComponent<CanvasGroup>();
+        canvasGroup.blocksRaycasts = false;
+
+        Image draggingImage = draggingobject.AddComponent<Image>(); // 드래그 중인 아이콘의 게임 오브젝트에 이미지 컴포넌트를 붙인다
+
+        // 본래 아이콘과 동일한 이미지와 외양을 설정
+        draggingImage.sprite = sourceImage.sprite;
+        draggingImage.rectTransform.sizeDelta = sourceImage.rectTransform.sizeDelta;
+        draggingImage.color = sourceImage.color;
+        draggingImage.material = sourceImage.material;
+
+        // 캔버스의 렉트 트렌스폼을 보존
+        canvasRectTransform = draggingImage.canvas.transform as RectTransform;
+        //드래그 중인 아이콘의 위치를 갱신
+        UpdateDraggingobjectPos(pointerEventData);
+    }
+
+    public void OnDrag(PointerEventData pointerEventData) // 드래그 조작 중에 발생하는 이벤트 메소드
+    {
+        UpdateDraggingobjectPos(pointerEventData);
+    }
+
+    public void OnEndDrag(PointerEventData pointerEventData) // 드래그 조작이 끝났을때 발생하는 이벤트 메소드
+    {
+        Destroy(draggingobject);
+    }
+}
---
+++ DandD/Assets/Droppable.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class Droppable : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPointerExitHandler
+{
+    [SerializeField] private Image iconeImage; // 드롭 영역에 표시되어있는 아이콘의 이미지를 가져옵니다
+    [SerializeField] private Color highlightedColor; // 드롭 영역에 표시되어있는 아이콘의 하이라이트 색을 가져옵니다
+    private Color normalColor; // 드롭 영역에 표시되어있는 아이콘의 원래 색
+
+    void Start()
+    {
+        normalColor = iconeImage.color; // 드롭 영역에 표시되어있는 아이콘의 원래 색을 보존
+    }
+
+    public void OnPointerEnter(PointerEventData pointerEventData) // 마우스 커서가 드롭 영역에 들어왔을때 호출
+    {
+        if(pointerEventData.dragging)
+        {
+            // 드래그 중이며 드롭영역에 마우스 커서가 들어왔을때
+            // 드롭 영역에 표시된 아이콘의 색을 하이라이트 색으로 변경
+            iconeImage.color = highlightedColor; 
+        }
+    }
+
+    public void OnPointerExit(PointerEventData pointerEventData) // 마우스 커서가 드롭 영역을 벗어날 때 호출
+    {
+        if (pointerEventData.dragging)
+        {
+            // 드래그 중이며 드롭영역에 마우스 커서가 벗어날때
+            // 드롭 영역에 표시된 아이콘의 색을 원래 색으로 변경
+            iconeImage.color = normalColor;
+        }
+    }
+
+    public void OnDrop(PointerEventData pointerEventData) // 드래그 하던 아이콘이 드랍됐을때 호출
+    {
+        Image droppedImage = pointerEventData.pointerDrag.GetComponent<Image>(); // 드래그 하고 있던 아이콘의 이미지 컴포넌트를 가져온다.
+
+        // 드롭 영역에 표시되어 있는 아이콘의 이미지를
+        // 드롭된 아이콘과 동일한 이미지로 변경하고 색을 원래 색으로 되돌린다.
+        iconeImage.sprite = droppedImage.sprite;
+        iconeImage.color = normalColor;
+    }
+}
